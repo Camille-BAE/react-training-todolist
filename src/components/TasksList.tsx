43bc62ebbde8023b1fc94bcfcfc5f3f1c9b177b4
Camille-BAE <camille.jbae@gmail.com>
2023-01-20 17:07:06 +0100

    added taskslist component

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,2 +1,19 @@
 import React from "react";
+import { TaskType } from "../models/Task"; // pour appeler l'interface
+import { Task } from "./Task";
+import "./TasksList.css"
 
+
+type TaskListProps = {
+  tasks : TaskType[];
+}
+
+export const TasksList = ({tasks}: TaskListProps) => {
+  return (
+    <div className="list-container">
+      {tasks.map((task) =>
+        <Task task={task}/>
+      )}
+    </div>
+  );
+};
